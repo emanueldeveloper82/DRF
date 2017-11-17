494d5ba82ed5cad526615f0416297b842dea4f1e
Emanuel da Anunciação Silva <emanuel.silva@w6solutions.com.br>
2017-11-17 14:14:12 -0300

    Tutorial 4  DRF completo nesta branch

--- Tutorial/snippets/urls.py
+++ Tutorial/snippets/urls.py
@@ -3,10 +3,17 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from django.conf.urls import include
 
-urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),    
-    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+urlpatterns = [    
+    url(r'^snippets/$', views.SnippetList.as_view()),    
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)   
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
